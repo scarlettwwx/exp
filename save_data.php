58749b31433e3e35fd57d667a478f1a769683814
Scarlett Xu <wenwen@Scarletts-MacBook-Pro.local>
2020-10-27 11:25:01 -0400

    save data

---
+++ save_data.php
@@ -0,0 +1,8 @@
+<?php
+$post_data = json_decode(file_get_contents('php://input'), true); 
+// the directory "data" must be writable by the server, use my github repo or local?
+$name = "https://scarlettwwx.github.io/data/".$post_data['filename'].".csv"; 
+$data = $post_data['filedata'];
+// write the file to disk/directory
+file_put_contents($name, $data);
+?>
